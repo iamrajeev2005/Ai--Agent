c82daad8ddd6a7e94cf5b6fe2764722cd871743a
Rajeev <fea.rajeev@gmail.com>
2025-09-17 17:14:25 +0530

    build ai agent

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
-console.log("Hello via Agent!");
+import { ChatGroq } from "@langchain/groq";
+import {
+  MemorySaver,
+  MessagesAnnotation,
+  StateGraph,
+} from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { TavilySearch } from "@langchain/tavily";
+import readline from "node:readline/promises";
+
+const checkPointer = new MemorySaver();
+
+const tool = new TavilySearch({
+  maxResults: 3,
+  topic: "general",
+  // includeAnswer: false,
+  // includeRawContent: false,
+  // includeImages: false,
+  // includeImageDescriptions: false,
+  // searchDepth: "basic",
+  // timeRange: "day",
+  // includeDomains: [],
+  // excludeDomains: [],
+});
+
+const tools = [tool];
+const toolNode = new ToolNode(tools);
+
+function shouldContinue(state) {
+  const lastMessage = state.messages[state.messages.length - 1];
+  if (lastMessage?.tool_calls?.length > 0) {
+    return "tools";
+  }
+  return "__end__";
+  // console.log(state)
+}
+const llm = new ChatGroq({
+  model: "openai/gpt-oss-120b",
+  temperature: 0,
+  maxTokens: undefined,
+  maxRetries: 2,
+}).bindTools(tools);
+
+async function callModel(state) {
+  //   console.log("LLM Calling.....");
+
+  const response = await llm.invoke(state.messages);
+  return { messages: [response] };
+}
+
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode("agent", callModel)
+  .addNode("tools", toolNode)
+  .addEdge("__start__", "agent")
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue);
+
+const app = workflow.compile({ checkpointer: checkPointer });
+
+async function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const thread_id ="1"
+
+  while (true) {
+      const userInput = await rl.question("You:");
+    if (userInput === "/tata") break;
+      const finalInput = await app.invoke(
+      {
+        messages: [{ role: "user", content: userInput }],
+      },
+      { configurable: { thread_id } }
+    );
+    const lastMessage = finalInput.messages[finalInput.messages.length - 1];
+    console.log("AI:", lastMessage?.content);
+  }
+
+  rl.close();
+}
+
+main();
